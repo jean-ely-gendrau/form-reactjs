0eabf23139ee393014993ffdf58ac4475ccd9b51
jean-ely.gendrau <150784008+jean-ely-gendrau@users.noreply.github.com>
2026-01-15 16:15:01 +0100

    Ajout du composant ConnectForm et suppression du formulaire dans App

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import { useState } from 'react'
 import './App.css'
-import InputForm from './components/InputForm'
+import { ConnectForm } from './components/ConnectForm';
 
 function App() {
-  const [nom, setNom] = useState("");
 
-  // Fonction appelée lors de la soumission du formulaire
-  const handleSubmit = (e) => {
-    //Empêche le comportement par défaut du formulaire
-    e.preventDefault();
-
-    // Affiche dans la console les données saisies par l'utilisateur
-    // Ces valeurs proviennent généralement du state (useState)
-    console.log({
-      nom: nom
-    })
-  }
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <InputForm
-          label="nom"
-          onChange={(e) => setNom(e.target.value)}
-          value={nom}
-        />
-
-        <button type="submit">Evoyer</button>
-      </form>
+      <ConnectForm />
     </>
   )
 }
---
+++ src/components/ConnectForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import InputForm from "./InputForm";
+
+export function ConnectForm() {
+
+    const [email, setEmail] = useState("");
+
+    // Fonction appelée lors de la soumission du formulaire
+    const handleSubmit = (e) => {
+        //Empêche le comportement par défaut du formulaire
+        e.preventDefault();
+
+        // Affiche dans la console les données saisies par l'utilisateur
+        // Ces valeurs proviennent généralement du state (useState)
+        console.log({
+            email: email
+        })
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <InputForm
+                    label="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+
+                <button type="submit">Evoyer</button>
+            </form>
+        </>
+    )
+}
